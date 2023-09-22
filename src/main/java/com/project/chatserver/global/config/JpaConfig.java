c43faa8bd510b61a143ce944d7a66722e84b0e2a
grace <grace994084@gmail.com>
2023-09-22 13:13:38 +0900

    feat : jpa, redis config 설정

---
+++ src/main/java/com/project/chatserver/global/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package com.project.chatserver.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = {"com.project.chatserver.domain.*.repository.jpa"})
+public class JpaConfig {
+}
